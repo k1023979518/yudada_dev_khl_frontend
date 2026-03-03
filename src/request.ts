e9b0e96c436155a64c591ebe46efab268ba796b0
k1023979518 <1023979518@qq.com>
2026-03-03 15:56:33 +0800

    初始化前端项目

--- src/request.ts
+++ src/request.ts
@@ -1,8 +1,12 @@
 import axios from "axios";
 import { Message } from "@arco-design/web-vue";
 
+export const isDev: boolean = process.env.NODE_ENV === "development";
+
 const myAxios = axios.create({
-  baseURL: "http://localhost:8101",
+  baseURL: isDev
+    ? "http://localhost:8101"
+    : "https://aiprojectbackend-228626-4-1407294221.sh.run.tcloudbase.com/",
   timeout: 60000,
   withCredentials: true,
 });
