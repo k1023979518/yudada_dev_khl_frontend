8d75e45646b0f9f26144098dd2ecdebf46ac9350
k1023979518 <1023979518@qq.com>
2026-04-08 15:08:59 +0800

    前端bug修复

--- src/layouts/BasicLayout.vue
+++ src/layouts/BasicLayout.vue
@@ -7,11 +7,7 @@
       <a-layout-content class="content">
         <router-view />
       </a-layout-content>
-      <a-layout-footer class="footer">
-        <a href="https://www.code-nav.cn" target="_blank">
-          编程导航 by 程序员鱼皮
-        </a>
-      </a-layout-footer>
+      <a-layout-footer class="footer"> </a-layout-footer>
     </a-layout>
   </div>
 </template>
--- src/views/app/AppDetailPage.vue
+++ src/views/app/AppDetailPage.vue
@@ -16,26 +16,18 @@
                   :image-url="data.user?.userAvatar"
                   :style="{ marginRight: '8px' }"
                 />
-                <a-typography-text
-                  >{{ data.user?.userName ?? "无名" }}
+                <a-typography-text>
+                  {{ data.user?.userName ?? "无名" }}
                 </a-typography-text>
               </div>
             </a-space>
           </p>
-          <p>
-            创建时间：{{ dayjs(data.createTime).format("YYYY-MM-DD HH:mm:ss") }}
-          </p>
+          <p>创建时间：{{ dayjs(data.createTime).format("YYYY-MM-DD HH:mm:ss") }}</p>
           <a-space size="medium">
-            <a-button type="primary" :href="`/answer/do/${id}`"
-              >开始答题
-            </a-button>
+            <a-button type="primary" @click="doStartAnswer">开始答题</a-button>
             <a-button @click="doShare">分享应用</a-button>
-            <a-button v-if="isMy" :href="`/add/question/${id}`"
-              >设置题目
-            </a-button>
-            <a-button v-if="isMy" :href="`/add/scoring_result/${id}`"
-              >设置评分
-            </a-button>
+            <a-button v-if="isMy" :href="`/add/question/${id}`">设置题目</a-button>
+            <a-button v-if="isMy" :href="`/add/scoring_result/${id}`">设置评分</a-button>
             <a-button v-if="isMy" :href="`/add/app/${id}`">修改应用</a-button>
           </a-space>
         </a-col>
@@ -64,26 +56,18 @@ interface Props {
 }
 
 const props = withDefaults(defineProps<Props>(), {
-  id: () => {
-    return "";
-  },
+  id: "",
 });
 
 const router = useRouter();
-
 const data = ref<API.AppVO>({});
-
-// 获取登录用户
 const loginUserStore = useLoginUserStore();
-let loginUserId = loginUserStore.loginUser?.id;
-// 是否为本人创建
+
+const loginUserId = computed(() => loginUserStore.loginUser?.id);
 const isMy = computed(() => {
-  return loginUserId && loginUserId === data.value.userId;
+  return !!loginUserId.value && loginUserId.value === data.value.userId;
 });
 
-/**
- * 加载数据
- */
 const loadData = async () => {
   if (!props.id) {
     return;
@@ -92,39 +76,30 @@ const loadData = async () => {
     id: props.id as any,
   });
   if (res.data.code === 0) {
-    data.value = res.data.data as any;
+    data.value = res.data.data as API.AppVO;
   } else {
-    message.error("获取数据失败，" + res.data.message);
+    message.error("获取数据失败：" + res.data.message);
   }
 };
 
-/**
- * 监听 searchParams 变量，改变时触发数据的重新加载
- */
 watchEffect(() => {
   loadData();
 });
 
-// 分享弹窗的引用
 const shareModalRef = ref();
-
-// 分享链接
 const shareLink = `${window.location.protocol}//${window.location.host}/app/detail/${props.id}`;
 
-// 分享
+const doStartAnswer = () => {
+  router.push(`/answer/do/${props.id}`);
+};
+
 const doShare = (e: Event) => {
-  if (shareModalRef.value) {
-    shareModalRef.value.openModal();
-  }
-  // 阻止冒泡，防止跳转到详情页
+  shareModalRef.value?.openModal?.();
   e.stopPropagation();
 };
 </script>
 
 <style scoped>
-#appDetailPage {
-}
-
 #appDetailPage .content-wrapper > * {
   margin-bottom: 24px;
 }
